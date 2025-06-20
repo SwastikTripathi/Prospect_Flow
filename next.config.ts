ea405513ea2abd51a7cbd12726ff2e84580a14ff
SwastikTripathi <88632085+SwastikTripathi@users.noreply.github.com>
2025-06-20 23:48:04 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,27 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '4kwallpapers.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
